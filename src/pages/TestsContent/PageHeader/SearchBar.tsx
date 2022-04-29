a5d8931ab8fd3075202430ddf3301357dd572ec9
Thais Franca <tfrancag@gmail.com>
2022-04-29 14:17:45 -0300

    feat. Changed searchBar to include autocomplete feature

--- src/pages/TestsContent/PageHeader/SearchBar.tsx
+++ src/pages/TestsContent/PageHeader/SearchBar.tsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 
-import { useAutocomplete } from "@mui/base/AutocompleteUnstyled";
-import { styled } from "@mui/material/styles";
 import * as style from "./style";
 import api from "../../../services/api";
 import useAuth from "../../../hooks/userAuth";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Autocomplete from "@mui/material/Autocomplete";
+import { useAutocomplete } from "@mui/base/AutocompleteUnstyled";
+import { styled } from "@mui/material/styles";
+
 interface Props {
   filter: null | string;
 }
@@ -37,22 +41,24 @@ export default function SearchBar(props: Props) {
   } = useAutocomplete({
     id: "use-autocomplete-demo",
     options: filterItems,
-    getOptionLabel: (option) => option.title,
+    getOptionLabel: (option) => option.name,
   });
 
   return (
     <style.SearchBarContainer>
-      <div {...getRootProps()}>
-        <Label {...getInputLabelProps()}>Search by {filter}</Label>
-        <Input {...getInputProps()} />
+      <div>
+        <div {...getRootProps()}>
+          <Label {...getInputLabelProps()}>Search by {filter}</Label>
+          <Input {...getInputProps()} />
+        </div>
+        {groupedOptions.length > 0 ? (
+          <Listbox {...getListboxProps()}>
+            {(groupedOptions as typeof filterItems).map((option, index) => (
+              <li {...getOptionProps({ option, index })}>{option.name}</li>
+            ))}
+          </Listbox>
+        ) : null}
       </div>
-      {groupedOptions.length > 0 ? (
-        <Listbox {...getListboxProps()}>
-          {(groupedOptions as typeof filterItems).map((option, index) => (
-            <li {...getOptionProps({ option, index })}>{option.name}</li>
-          ))}
-        </Listbox>
-      ) : null}
     </style.SearchBarContainer>
   );
 }
@@ -62,13 +68,17 @@ const Label = styled("label")({
 });
 
 const Input = styled("input")(({ theme }) => ({
-  width: 200,
-  backgroundColor: theme.palette.background.paper,
+  width: 500,
+  height: 50,
+  backgroundColor: "#9575cd",
+  borderRadius: "5px",
+  border: "1px solid #e36dee61",
+
   color: theme.palette.getContrastText(theme.palette.background.paper),
 }));
 
 const Listbox = styled("ul")(({ theme }) => ({
-  width: 200,
+  width: 500,
   margin: 0,
   padding: 0,
   zIndex: 1,
@@ -77,9 +87,9 @@ const Listbox = styled("ul")(({ theme }) => ({
   backgroundColor: theme.palette.background.paper,
   overflow: "auto",
   maxHeight: 200,
-  border: "1px solid rgba(0,0,0,.25)",
+  border: "1px solid #e36dee61",
   '& li[data-focus="true"]': {
-    backgroundColor: "#4a8df6",
+    backgroundColor: "#9575cd",
     color: "white",
     cursor: "pointer",
   },
